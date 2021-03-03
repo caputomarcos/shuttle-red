96a0d848bf2804591ff6878ac9baaa0692e82ab8
2WeltenChris <c.geiger@lohrmann.de>
2021-03-03 15:25:52 +0000

    Just some very small code snippets

--- src/shuttle-receive.js
+++ src/shuttle-receive.js
@@ -2,6 +2,9 @@ module.exports = function (RED) {
   function ShuttleReceiveNode (config) {
     RED.nodes.createNode(this, config)
     // const node = this
+    process.on('message', (m) => {
+      console.log('Received message:', m)
+    })
   }
   RED.nodes.registerType('shuttle-receive', ShuttleReceiveNode)
 }
--- src/shuttle-send.js
+++ src/shuttle-send.js
@@ -2,6 +2,8 @@ module.exports = function (RED) {
   function ShuttleSendNode (config) {
     RED.nodes.createNode(this, config)
     // const node = this
+    // Send from child process
+    process.send({ foo: 'bar', baz: NaN })
   }
   RED.nodes.registerType('shuttle-send', ShuttleSendNode)
 }
